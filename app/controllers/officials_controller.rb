068f1051865557529bceac792a2b368ead7d977d
Joe Winter <joe@joeventures.com>
2016-03-05 13:39:47 -0500

    Add officials routes and controller

---
+++ app/controllers/officials_controller.rb
@@ -0,0 +1,29 @@
+class OfficialsController < ApplicationController
+  def index
+    #todo
+  end
+
+  def new
+    #todo
+  end
+
+  def create
+    #todo
+  end
+
+  def show
+    #todo
+  end
+
+  def edit
+    #todo
+  end
+
+  def update
+    #todo
+  end
+
+  def destroy
+    #todo
+  end
+end
