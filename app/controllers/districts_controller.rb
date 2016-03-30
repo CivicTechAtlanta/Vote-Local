c8b428253164eeb95d639bafef4ca6f754570a8a
Andrew Vorakrajangthiti <andrewjvora@gmail.com>
2016-03-29 20:40:17 -0400

    Checking for whether @districts or @elections is nil before trying to use them.

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,3 +1,5 @@
+
+
 class DistrictsController < ApplicationController
   def index
     #todo
@@ -5,6 +7,7 @@ class DistrictsController < ApplicationController
 
   def new
     #todo
+    @district = nil
   end
 
   def create
@@ -31,5 +34,44 @@ class DistrictsController < ApplicationController
   private
   def district_params
     params.require(:name).permit(:type, :description)
+  end
+
+  def find_district(point, map)
+    district_array = map.features
+
+    district_array.length.times do |i|
+      polygonCoords = district_array[i].geometry.coordinates[0]
+
+      if pn_poly(point, polygonCoords)
+        return district_array
+    end
+  end
+
+  def pn_poly (point, polygonCoords)
+    # ray-casting algorithm based on
+    # http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    x = point[0]
+    y = point[1]
+
+    inside = false
+    numCoords = polygonCoords.length
+
+    j = numCoords - 1
+    numCoords.times do |i|
+      xi = [i][0]
+      yi = vs[i][1]
+
+      xj = vs[j][0]
+      yj = vs[j][1]
+
+      intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+      if intersect
+        inside = !inside
+      end
+
+      j = i + 1
+    end
+
+    return inside
   end
 end
